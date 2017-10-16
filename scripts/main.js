f2fe07a18de72dab9cb488b4246dd8164e6f27ea
David Willis <dwillislc@gmail.com>
2017-10-16 11:17:25 -0400

    Updates to get search working
    Updated display, pagination and search function so that links work correctly.

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,24 @@
 const $studentList = $('.student-list'); 
 const $students = $('.student-item');
 const $pagination = $('.pagination'); 
-
+const currentArray = $students.slice(0);  
 //Create an array of student names and emails
-const $studentNames = $students.find('h3')
-const $studentEmails = $students.find('.email')
+const $studentNames = $students.find('h3');
+const $studentEmails = $students.find('.email');
 
 //Function displays a range of 10 students based on the number inputted in the parameter
-function displayStudents (num, array) {
-	$studentList.empty(); 
-	let studentArray = $students; 
-	if (array != null) {
-		studentArray = array; 
-	} 
+function displayStudents (num) {
+	$studentList.empty();  
+
 	// If student list less than 10, display them 
-	if (studentArray.length < 10) {
-		for (let i = 0; i < studentArray.length; i++) {
-			$studentList.append(studentArray[i]);
+	if (currentArray.length < 10) {
+		for (let i = 0; i < currentArray.length; i++) {
+			$studentList.append(currentArray[i]);
 		}
 	} else {
 		let nextNums = (num - 1) * 10;
-		for (let i = nextNums; i < nextNums + 9; i++) {
-			$studentList.append(studentArray[i]); 
+		for (let i = nextNums; i < nextNums + 10; i++) {
+			$studentList.append(currentArray[i]); 
 		}		
 	}	 
 }
@@ -31,31 +28,31 @@ function createPagination (students) {
 	if (students.length < 10) {
 		return; 
 	}
-
 	let numPages = students.length/10; 
 	for (let i = 1; i < Math.ceil(numPages)+1; i++) {
 		$pagination.append("<li><a href='#'>"+ i +"</a></li>"); 
 	}
+
+	linkClick(); 
 }
 
+//Link to the correct user list
+function linkClick () {
+	let $link = $pagination.find('a'); 
+	$link.on('click', function () {
+		let num = parseInt($(this).html());
+		//Give link active class
+		$('a.active').removeClass("active");
+		$(this).addClass("active"); 
+		displayStudents(num); 
+	});
+}
+
+
+
 //Display the first ten students when user enters the webpage 
-displayStudents(1) 
-createPagination($students)
-
-//Select all links in the pagination div
-const $links = $pagination.find('a');
-
-//Display the appropriate range of ten based on the clicked link 
-$links.on('click', function () {
-	let num = parseInt($(this).html());
-	
-	//Give link active class
-	$('a.active').removeClass("active");
-	$(this).addClass("active"); 
-	 
-	displayStudents(num); 
-});
-
+displayStudents(1);
+createPagination($students);
 
 /******
 Search bar will find names or emails that match user's input. 
@@ -67,47 +64,61 @@ try searching again.
 $('.page-header').append('<form class="student-search"><input type="text"><button type="button">Search</button></form>');  
 
 //Assign search bar to variable
-const $userInput = $('.student-search');  
+const $userInput = $('.student-search');
+const matched = [];  
 
 // Find all students with a name or email that matches search input
 function searchList() {
 	//Get input value 
 	let $input = $userInput.find('input').val();
-	
-	//Don't run function if theres no value in search bar
-	if ($input == ''){
-		displayStudents(null, $students)
-	}
+	currentArray.length = 0;
 
 	//Remove the page link 
 	$pagination.empty(); 
 
-	//Loop through list and input matched searches into the array 
-	let matched = [];
+	//Return original student list if nothing is searched
+	if ($input === ''){
+		for (let i = 0; i < $students.length; i++) {
+			currentArray.push($students[i]); 
+		}
+		createPagination(currentArray);
+		displayStudents(1); 
+		return;
+	}
+
+	// Reset matched array  
+	matched.length = 0; 
+
+	//Loop through list and input matched searches into the array
 	for (let i = 0; i < $studentNames.length; i ++) {
 		if ($studentNames[i].innerHTML.includes($input) || $studentEmails[i].innerHTML.includes($input)) {
 			matched.push($students[i]); 
 		}
 	}
 	
-	//Alert if no matches found 
-	if (matched.length == 0){
+	//Add matched elements to current array 
+	for (let i = 0; i < matched.length; i++) {
+			currentArray.push(matched[i]); 
+		} 
+
+	// Create pagination if there are more than ten matches  
+	if (matched.length === 0){
 		$studentList.empty();
 		$studentList.append("There were no students that matched your search. Please try again.");
 		return; 
-	}	
-
-	//Add pagination if over 10 students found
-	if (matched.length < 10){
-		displayStudents(null, matched); 
+	} else if (matched.length < 10) {
+		displayStudents(); 
 	} else {
 		createPagination(matched); 
-		displayStudents(1, matched); 
+		displayStudents(1); 
 	}
+	
 }
 
 //Run search function when user clicks search button 
 $userInput.find('button').on('click', function () {
 	searchList(); 
 });
+
+
 
